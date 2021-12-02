78acd8898a9e1b3567924c776af88c2289bc2569
Denys Kostiuk <93026512+DenKostiuk@users.noreply.github.com>
2021-12-02 16:22:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let num = +prompt('Введите число');
 
-let num1 = num > Math.PI ? true : false;
-let num2 = num < Math.PI ? true : false;
-let num3 = num === Math.PI ? true : false;
+let num1 = num > +Math.PI.toFixed(2) ? true : false;
+let num2 = num < +Math.PI.toFixed(2) ? true : false;
+let num3 = num === +Math.PI.toFixed(2) ? true : false;
 
-console.log('You entered: ' +num);
-console.log('Is greater then PI: ' +num1);
-console.log('Is less then PI: ' +num2);
-console.log('Is equal PI: ' +num3);
+console.log('You entered: ' + num);
+console.log('Is greater then PI: ' + num1);
+console.log('Is less then PI: ' + num2);
+console.log('Is equal PI: ' + num3);
